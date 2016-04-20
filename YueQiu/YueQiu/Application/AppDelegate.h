93c758a0c5ae96addf6be8b3c4c2eb5c1f46e59c
章鱼 <16118707@qq.com>
2016-04-20 15:01:03 +0800

    更正版本

---
+++ YueQiu/YueQiu/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YueQiu
+//
+//  Created by 章鱼 on 16/4/20.
+//  Copyright © 2016年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

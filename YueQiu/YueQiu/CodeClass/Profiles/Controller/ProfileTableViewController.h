7f2e73949961820bfff1e75e9877de7f6ab02e95
章鱼 <16118707@qq.com>
2016-04-20 15:22:16 +0800

    更正

---
+++ YueQiu/YueQiu/CodeClass/Profiles/Controller/ProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileTableViewController.h
+//  YueQiu
+//
+//  Created by 章鱼 on 16/4/20.
+//  Copyright © 2016年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileTableViewController : UITableViewController
+
+@end
